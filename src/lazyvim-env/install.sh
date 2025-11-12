d02d07740ac46772f1ac2d316152e0401dc12318
Loïc Guitaut <flink@belfalas.eu>
2025-11-12 23:53:51 +0100

    Add homebrew and lazyvim-env features

---
+++ src/lazyvim-env/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+sudo -u \#1000 -iH brew install neovim luarocks ripgrep claude-code lazygit tree-sitter-cli fd ag btop bat fzf ast-grep
+
+if command -v gem &>/dev/null; then
+  gem install syntax_tree
+else
+  sudo -u \#1000 -iH brew install ruby
+  sudo -u \#1000 -iH gem install syntax_tree
+fi
+
+if command -v node &>/dev/null; then
+  echo "Node is already installed"
+else
+  sudo -u \#1000 -iH brew install node
+fi
