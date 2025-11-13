b3bfa9fe261d19c9b6866cf7a28f62ef6f539ecc
Loïc Guitaut <flink@belfalas.eu>
2025-11-13 23:26:54 +0100

    Add neovim gem

--- src/lazyvim-env/install.sh
+++ src/lazyvim-env/install.sh
@@ -3,10 +3,10 @@
 sudo -u \#1000 -iH brew install neovim luarocks ripgrep claude-code lazygit tree-sitter-cli fd ag btop bat fzf ast-grep
 
 if command -v gem &>/dev/null; then
-  gem install syntax_tree
+  gem install syntax_tree neovim
 else
   sudo -u \#1000 -iH brew install ruby
-  sudo -u \#1000 -iH gem install syntax_tree
+  sudo -u \#1000 -iH gem install syntax_tree neovim
 fi
 
 if command -v node &>/dev/null; then
